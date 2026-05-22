7a8cf147580cee2ac15cee10afe2827cafe68c7f
Martin Mulyosyahidin <martinms@MacBook-Pro-Martin.local>
2026-05-22 10:17:13 +0700

    Setup Traefik Deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,16 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Kasir 1',
-            'email' => 'kasir1@apotek.app',
-            'password' => bcrypt('password'),
-            'email_verified_at' => now(),
-        ]);
-
-        User::factory()->create([
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@apotek.app',
             'role' => UserRole::ADMIN->value,
